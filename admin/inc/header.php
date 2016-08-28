5be41c7a932b1ab0ce65ce4a054b36ebf9fa1789
Fukotaku <contact@fukotaku.fr>
2016-08-28 13:43:41 +0200

    Update navbar

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -25,10 +25,14 @@
   <body>
 
     <nav class="navbar navbar-default">
-    <div class="container-fluid">
+    <div class="container">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        </button>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
         <a class="navbar-brand"><span class="glyphicon glyphicon-blackboard"></span> &nbsp;Files Lister : Panel administrateur</a>
       </div>
 
