1dc4563926b06f6e9a1361938e6d59bdde8c3005
Fukotaku <contact@fukotaku.fr>
2016-09-23 23:25:12 +0200

    Re-form database and add comments

--- inc/database.php
+++ inc/database.php
@@ -1,14 +1,18 @@
 <?php
 
-$user = "root";
-$pass = "root";
-
+// On essaye de se connecter à la base de données
 try{
-  $pdo = new PDO('mysql:host=localhost;dbname=filesmanager', $user, $pass);
+  // Les informations de la base de données
+  $pdo = new PDO('mysql:host=localhost;dbname=filesmanager', 'root', 'root');
+
+  // Attribut pour définir les type de messages d'erreurs
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+
+  // Encodage des données récupérées
   $pdo->exec('SET NAMES utf8');
 
+// En cas d'échec, renvoyer l'erreur
 }catch(PDOException $e){
   print "Erreur : " . $e->getMessage() . "<br/>";
   die();
