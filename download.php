1eed082b79f0938ca69f8b12f8ec20c343ae22fd
simon.micheneau <simon.micheneau@linexos.fr>
2016-12-13 14:23:11 +0100

    Update download and footer file

---
+++ download.php
@@ -0,0 +1,22 @@
+<?php
+  require_once 'inc/database.php';
+  require_once 'inc/function.php';
+
+  if(isset($_GET['id'])){
+    $id = $pdo->quote($_GET['id']);
+    $req = $pdo->query("SELECT * FROM files WHERE id = $id");
+    $fileDetect = false;
+    while($data = $req->fetch()){
+      $fileName = $data->name;
+      $dirNumber = $data->id_directory;
+      $fileDetect = true;
+    }
+    if($fileDetect == true){
+      $directory = "/admin/directory/".$dirNumber."/".$fileName;
+      header('Content-Type: application/octet-stream');
+      header('Content-Transfer-Encoding: Binary');
+      header('Content-disposition: attachment; filename="' . basename($directory) . '"');
+    }
+  }
+
+?>
