fbf08c8835f70c8fb733d714d60797532c2362e5
simon.micheneau <simon.micheneau@linexos.fr>
2016-12-14 12:29:15 +0100

    Update settings page

--- admin/settings.php
+++ admin/settings.php
@@ -3,6 +3,29 @@
   require_once 'inc/header.php';
   is_admin();
 
+  if(!empty($_POST)){
+    if(!empty($_POST['size'])){
+      if($_POST['format'] === 'mo'){
+        $size = moConvert($_POST['size']);
+        $req = $pdo->prepare("UPDATE settings SET upload_size = ?");
+        $req->execute([$size]);
+
+      }elseif($_POST['format'] === 'go'){
+        $size = goConvert($_POST['size']);
+        $req = $pdo->prepare("UPDATE settings SET upload_size = ?");
+        $req->execute([$size]);
+      }
+
+      $_SESSION['flash']['success'] = 'Les paramètres on bien étais enregistrés';
+      header('location: settings.php');
+      exit();
+    }else{
+      $_SESSION['flash']['danger'] = 'Le champ "Taille d\'upload max" est vide';
+      header('location: settings.php');
+      exit();
+    }
+  }
+
 ?>
 
   <div class="container">
@@ -22,16 +45,19 @@
     <div class="jumbotron">
       <form class="form-group" action="" method="post">
         <br/>
-        <p>Gestion des uploads :</p>
-          <p>Modifier la taille max des fichiers uploadés.</p>
+        <p>Modifier la taille max des fichiers uploadés:</p>
           <label for="select" class="control-label">Format de taille :</label>
           <select class="form-control" id="select" name="format">
-            <option value="mo">Mo</option>
+            <option value="mo" selected>Mo</option>
             <option value="go">Go</option>
           </select>
           <br/>
+          <?php
+            $req = $pdo->query("SELECT * FROM settings");
+            $settings = $req->fetch();
+          ?>
           <label class="control-label" for="maxSize">Taille d'upload max :</label>
-          <input class="form-control" id="maxSize" value="" type="number" name="size">
+          <input class="form-control" id="maxSize" value="<?php echo octetConvertToMo($settings->upload_size); ?>" type="number" name="size">
 
           <hr>
 
